fb4e4c298055418409e0b5754633314f74718d14
Daniel Fink <daniel-fink@outlook.com>
2020-01-15 20:37:26 +0100

    Implemented first version of wu palmer

--- taxonomie.py
+++ taxonomie.py
@@ -14,7 +14,7 @@ from attribute import Attribute
 class Taxonomie():
     def __init__(self) -> None:
         self.database: Database = Database()
-        self.plot_datatype: str = "png"
+        self.plot_datatype: str = "svg"
         self.plot_directory: str = "plots"
         self.graph_directory: str = "graphs"
         self.color: Graph = None
@@ -24,12 +24,26 @@ class Taxonomie():
         self.gender: Graph = None
         self.age_impression: Graph = None
         self.genre: Graph = None
+        self.all_taxonomie_name = "Taxonomie"
+        self.all: Graph = None
         return
 
     def __del__(self) -> None:
         del self.database
         return
     
+    def merge(self, root_name: str, *graphs: [Graph]) -> Graph:
+        graph = nx.DiGraph()
+        graph.add_node(root_name)
+
+        for subgraph in graphs:
+            graph.add_edges_from(subgraph.edges(data = True))
+            graph.add_nodes_from(subgraph.nodes(data = True))
+            subgraph_root_name = [n for n,d in subgraph.in_degree() if d == 0][0]
+            graph.add_edge(root_name, subgraph_root_name)
+
+        return graph
+
     # Loads the taxonomies from database
     def load_from_database(self) -> None:
         self.database.open()
@@ -42,9 +56,17 @@ class Taxonomie():
         self.genre = self.database.get_genre()
         self.database.close()
 
-    # Gets the root of the given graph (tree)
-    def get_root(self, graph: Graph) -> str:
-        return [n for n,d in graph.in_degree() if d == 0][0]
+        self.all = self.merge(
+            self.all_taxonomie_name,
+            self.color,
+            self.traits,
+            self.condition,
+            self.stereotype,
+            self.gender,
+            self.age_impression,
+            self.genre)
+
+        return
 
     # Gets the graph corresponding to the attribute
     def get_graph(self, attribute: Attribute) -> Graph:
@@ -63,7 +85,7 @@ class Taxonomie():
         elif attribute == Attribute.genre:
             return self.genre
         else:
-            raise Exception("Unkown attribute")
+            raise Exception("Unkown attribute \"" + str(attribute) + "\"")
 
     # Sets the graph corresponding to the attribute
     def set_graph(self, attribute: Attribute, graph: Graph) -> None:
@@ -82,8 +104,17 @@ class Taxonomie():
         elif attribute == Attribute.genre:
             self.genre = graph
         else:
-            raise Exception("Unkown attribute")
+            raise Exception("Unkown attribute \"" + attribute + "\"")
         return
+
+    # Gets the root of the given graph (tree)
+    def get_root(self, attribute: Attribute) -> str:
+        graph = self.get_graph(attribute)
+        return [n for n,d in graph.in_degree() if d == 0][0]
+
+    # Gets the count of edges between first and second
+    def get_count_of_edges(self, attribute: Attribute, first: Any, sedonc: Any) -> int:
+        return 0
 
     # Safe the graph to the given attribute as dot 
     # and png file and displays the created image
@@ -93,17 +124,17 @@ class Taxonomie():
 
         name = attribute.name
 
-        filepath: str = self.plot_directory + "/" + name
+        filepath: str = self.plot_directory + "\\" + name
 
         graph = self.get_graph(attribute)
 
         nx.nx_agraph.write_dot(graph, filepath + ".dot")
         subprocess.call(["dot", "-T" + self.plot_datatype, filepath + ".dot", "-o", filepath + "." + self.plot_datatype])
 
-        if display == True:
-            img = Image.open(filepath + "." + self.plot_datatype)
-            img.show()
-        
+        filename = os.getcwd() + "\\" + filepath + "." + self.plot_datatype
+
+        os.startfile(filename, 'open')
+
         return
     
     # Writes the graph to a data file
@@ -138,10 +169,56 @@ class Taxonomie():
 
         return
 
+    # Safes all graphs to data files
+    def safe_all_to_file(self) -> None:
+        tax.safe_to_file(Attribute.color)
+        tax.safe_to_file(Attribute.traits)
+        tax.safe_to_file(Attribute.condition)
+        tax.safe_to_file(Attribute.stereotype)
+        tax.safe_to_file(Attribute.gender)
+        tax.safe_to_file(Attribute.age_impression)
+        tax.safe_to_file(Attribute.genre)
+        return
+
+    # Loads all graphs to data files
+    def load_all_to_file(self) -> None:
+        tax.load_from_file(Attribute.color)
+        tax.load_from_file(Attribute.traits)
+        tax.load_from_file(Attribute.condition)
+        tax.load_from_file(Attribute.stereotype)
+        tax.load_from_file(Attribute.gender)
+        tax.load_from_file(Attribute.age_impression)
+        tax.load_from_file(Attribute.genre)
+        return
+
+    def wu_palmer_compare(self, attribute: Attribute, first: str, second: str) -> float:
+        # Get directed graph
+        d_graph = self.get_graph(attribute)
+
+        # Get undirected graph
+        ud_graph = d_graph.to_undirected()
+
+        # Get lowest reachable node from both
+        lowest_common_ancestor = nx.algorithms.lowest_common_ancestors.lowest_common_ancestor(d_graph, first, second)
+
+        # Get root of graph
+        root = self.get_root(attribute)
+
+        # Count edges - weight is 1 per default
+        d1 = nx.algorithms.shortest_paths.generic.shortest_path_length(ud_graph, first, lowest_common_ancestor)
+        d2 = nx.algorithms.shortest_paths.generic.shortest_path_length(ud_graph, second, lowest_common_ancestor)
+        d3 = nx.algorithms.shortest_paths.generic.shortest_path_length(ud_graph, lowest_common_ancestor, root)
+
+        return 2 * d3 / (d1 + d2 + 2* d3)
+
 if __name__ == '__main__':
     tax = Taxonomie()
     #tax.load_from_database()
-    #tax.safe_to_file(Attribute.gender)
-    tax.load_from_file(Attribute.gender)
-    tax.plot(Attribute.gender)
+    #tax.safe_all_to_file()
+    tax.load_from_file(Attribute.color)
+    tax.plot(Attribute.color)
+    print("Brauntöne - Orange: " + str(tax.wu_palmer_compare(Attribute.color, "Brauntöne", "Orange")))
+    print("Farben - Dunkelviolett: " + str(tax.wu_palmer_compare(Attribute.color, "Farben", "Dunkelviolett")))
+    print("Grüntöne - Farbe: " + str(tax.wu_palmer_compare(Attribute.color, "Grüntöne", "Farben")))
+
     del tax
