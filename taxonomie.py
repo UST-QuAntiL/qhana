6f598f7d2833e96e1cae482a2dbe23dda729023d
Daniel Fink <daniel-fink@outlook.com>
2020-01-15 12:03:40 +0100

    Implemented ability to store and loat graphs from json file

--- taxonomie.py
+++ taxonomie.py
@@ -7,13 +7,25 @@ import matplotlib.image as mpimg
 from PIL import Image
 import subprocess
 import os
+from networkx.readwrite import json_graph
+import simplejson as json
+import enum
+
+class Attribute(enum.Enum):
+    color = 1
+    traits = 2
+    condition = 3
+    stereotype = 4
+    gender = 5
+    age_impression = 6
+    genre = 7
 
 class Taxonomie():
     def __init__(self) -> None:
         self.database: Database = Database()
-        self.is_loaded: bool = False
         self.plot_datatype: str = "png"
         self.plot_directory: str = "plots"
+        self.graph_directory: str = "graphs"
         self.color: Graph = None
         self.traits: Graph = None
         self.condition: Graph = None
@@ -28,7 +40,7 @@ class Taxonomie():
         return
     
     # Loads the taxonomies from database
-    def load(self) -> None:
+    def load_from_database(self) -> None:
         self.database.open()
         self.color = self.database.get_color()
         self.traits = self.database.get_traits()
@@ -37,26 +49,62 @@ class Taxonomie():
         self.gender = self.database.get_gender()
         self.age_impression = self.database.get_age_impression()
         self.genre = self.database.get_genre()
-        self.is_loaded = True
+        self.database.close()
 
     # Gets the root of the given graph (tree)
     def get_root(self, graph: Graph) -> str:
         return [n for n,d in graph.in_degree() if d == 0][0]
 
-    # Safe the given graph as dot and png file and
-    # displays the created image
-    def plot(self, graph: Graph, display: bool = True, name: str = "") -> None:
-        if self.is_loaded == False:
-            print("Taxonomie is not loaded")
-            return
+    # Gets the graph corresponding to the attribute
+    def get_graph(self, attribute: Attribute) -> Graph:
+        if attribute == Attribute.color:
+            return self.color
+        elif attribute == Attribute.traits:
+            return self.traits
+        elif attribute == Attribute.condition:
+            return self.condition
+        elif attribute == Attribute.stereotype:
+            return self.stereotype
+        elif attribute == Attribute.gender:
+            return self.gender
+        elif attribute == Attribute.age_impression:
+            return self.age_impression
+        elif attribute == Attribute.genre:
+            return self.genre
+        else:
+            raise Exception("Unkown attribute")
 
+    # Sets the graph corresponding to the attribute
+    def set_graph(self, attribute: Attribute, graph: Graph) -> None:
+        if attribute == Attribute.color:
+            self.color = graph
+        elif attribute == Attribute.traits:
+            self.traits = graph
+        elif attribute == Attribute.condition:
+            self.condition = graph
+        elif attribute == Attribute.stereotype:
+            self.stereotype = graph
+        elif attribute == Attribute.gender:
+            self.gender = graph
+        elif attribute == Attribute.age_impression:
+            self.age_impression = graph
+        elif attribute == Attribute.genre:
+            self.genre = graph
+        else:
+            raise Exception("Unkown attribute")
+        return
+
+    # Safe the graph to the given attribute as dot 
+    # and png file and displays the created image
+    def plot(self, attribute: Attribute, display: bool = True) -> None:
         if os.path.isdir(self.plot_directory) == False:
             os.mkdir(self.plot_directory)
 
-        if name == "":
-            name = self.get_root(graph)
+        name = attribute.name
 
         filepath: str = self.plot_directory + "/" + name
+
+        graph = self.get_graph(attribute)
 
         nx.nx_agraph.write_dot(graph, filepath + ".dot")
         subprocess.call(["dot", "-T" + self.plot_datatype, filepath + ".dot", "-o", filepath + "." + self.plot_datatype])
@@ -67,8 +115,42 @@ class Taxonomie():
         
         return
     
+    # Writes the graph to a data file
+    def safe_to_file(self,  attribute: Attribute) -> None:
+        if os.path.isdir(self.graph_directory) == False:
+            os.mkdir(self.graph_directory)
+
+        name = attribute.name
+
+        filename: str = self.graph_directory + "/" + name + ".json"
+
+        graph = self.get_graph(attribute)
+
+        graph_json = json_graph.node_link_data(graph)
+
+        json.dump(graph_json, open(filename,'w'), indent = 2)
+
+        return
+
+    # Reads the graph from a data file
+    def load_from_file(self, attribute: Attribute) -> None:
+        name = attribute.name
+
+        filename: str = self.graph_directory + "/" + name + ".json"
+
+        with open(filename) as f:
+            graph_json = json.load(f)
+
+        graph = json_graph.node_link_graph(graph_json)
+
+        self.set_graph(attribute, graph)
+
+        return
+
 if __name__ == '__main__':
     tax = Taxonomie()
-    tax.load()
-    tax.plot(tax.traits)
+    #tax.load_from_database()
+    #tax.safe_to_file(Attribute.gender)
+    tax.load_from_file(Attribute.gender)
+    tax.plot(Attribute.gender)
     del tax
