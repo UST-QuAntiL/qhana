345eb79865b14b7bd885b6ec5e2159fadb55953c
Daniel Fink <st141939@stud.uni-stuttgart.de>
2020-04-01 13:23:54 +0800

    Added more attributes for taxonomies.

--- backend/attribute.py
+++ backend/attribute.py
@@ -13,3 +13,12 @@ class Attribute(enum.Enum):
     # these are the attributes for the extended model
     job = 8
     times_of_day = 9
+    times_of_play = 10
+    role = 11
+    location_occurrence = 12
+    material = 13
+
+    # additional attributes
+    type_of_basic_element = 14
+    design = 15
+    color_concept = 16
--- backend/taxonomie.py
+++ backend/taxonomie.py
@@ -35,6 +35,15 @@ class Taxonomie(Singleton):
         # these are the taxonomies for the extended model
         self.job: Graph = None
         self.times_of_day: Graph = None
+        self.times_of_play: Graph = None
+        self.role: Graph = None
+        self.location_occurrence: Graph = None
+        self.material: Graph = None
+
+        # additional attributes
+        self.type_of_basic_element: Graph = None
+        self.design: Graph = None
+        self.color_concept: Graph = None
 
         self.all_taxonomie_name = "Taxonomie"
         self.all: Graph = None
@@ -69,7 +78,14 @@ class Taxonomie(Singleton):
             Attribute.age_impression: self.graph_directory + "/" + Attribute.age_impression.name + ".json",
             Attribute.genre: self.graph_directory + "/" + Attribute.genre.name + ".json",
             Attribute.job: self.graph_directory + "/" + Attribute.job.name + ".json",
-            Attribute.times_of_day: self.graph_directory + "/" + Attribute.times_of_day.name + ".json"
+            Attribute.times_of_day: self.graph_directory + "/" + Attribute.times_of_day.name + ".json",
+            Attribute.times_of_play: self.graph_directory + "/" + Attribute.times_of_play.name + ".json",
+            Attribute.role: self.graph_directory + "/" + Attribute.role.name + ".json",
+            Attribute.location_occurrence: self.graph_directory + "/" + Attribute.location_occurrence.name + ".json",
+            Attribute.material: self.graph_directory + "/" + Attribute.material.name + ".json",
+            Attribute.type_of_basic_element:self.graph_directory + "/" + Attribute.type_of_basic_element.name + ".json",
+            Attribute.design: self.graph_directory + "/" + Attribute.design.name + ".json",
+            Attribute.color_concept: self.graph_directory + "/" + Attribute.color_concept.name + ".json"
         }
 
         for attribute in filenames:
@@ -89,6 +105,7 @@ class Taxonomie(Singleton):
     # Loads the taxonomies from database
     def load_all_from_database(self) -> None:
         self.database.open()
+
         self.color = self.database.get_color()
         self.traits = self.database.get_traits()
         self.condition = self.database.get_condition()
@@ -98,6 +115,14 @@ class Taxonomie(Singleton):
         self.genre = self.database.get_genre()
         self.job = self.database.get_job()
         self.times_of_day = self.database.get_times_of_day()
+        self.times_of_play = self.database.get_times_of_play()
+        self.role = self.database.get_role()
+        self.location_occurrence = self.database.get_location_occurrence()
+        self.material = self.database.get_material()
+        self.type_of_basic_element = self.database.get_type_of_basic_element()
+        self.design = self.database.get_design()
+        self.color_concept = self.database.get_color_concept()
+
         self.database.close()
 
         #self.all = self.merge(
@@ -155,6 +180,13 @@ class Taxonomie(Singleton):
         self.safe_to_file(Attribute.genre)
         self.safe_to_file(Attribute.job)
         self.safe_to_file(Attribute.times_of_day)
+        self.safe_to_file(Attribute.times_of_play)
+        self.safe_to_file(Attribute.role)
+        self.safe_to_file(Attribute.location_occurrence)
+        self.safe_to_file(Attribute.material)
+        self.safe_to_file(Attribute.type_of_basic_element)
+        self.safe_to_file(Attribute.design)
+        self.safe_to_file(Attribute.color_concept)
 
         Logger.normal("All taxonomies have been saved as json to " + self.graph_directory)
 
@@ -171,6 +203,14 @@ class Taxonomie(Singleton):
         self.load_from_file(Attribute.genre)
         self.load_from_file(Attribute.job)
         self.load_from_file(Attribute.times_of_day)
+        self.load_from_file(Attribute.times_of_play)
+        self.load_from_file(Attribute.role)
+        self.load_from_file(Attribute.location_occurrence)
+        self.load_from_file(Attribute.material)
+        self.load_from_file(Attribute.type_of_basic_element)
+        self.load_from_file(Attribute.design)
+        self.load_from_file(Attribute.color_concept)
+
         return
 
     # Gets the graph corresponding to the attribute
@@ -193,6 +233,20 @@ class Taxonomie(Singleton):
             return self.job
         elif attribute == Attribute.times_of_day:
             return self.times_of_day
+        elif attribute == Attribute.times_of_play:
+            return self.times_of_play
+        elif attribute == Attribute.role:
+            return self.role
+        elif attribute == Attribute.location_occurrence:
+            return self.location_occurrence
+        elif attribute == Attribute.material:
+            return self.material
+        elif attribute == Attribute.type_of_basic_element:
+            return self.type_of_basic_element
+        elif attribute == Attribute.design:
+            return self.design
+        elif attribute == Attribute.color_concept:
+            return self.color_concept
         else:
             raise Exception("Unkown attribute \"" + str(attribute) + "\"")
 
@@ -216,6 +270,20 @@ class Taxonomie(Singleton):
             self.job = graph
         elif attribute == Attribute.times_of_day:
             self.times_of_day = graph
+        elif attribute == Attribute.times_of_play:
+            self.times_of_play = graph
+        elif attribute == Attribute.role:
+            self.role = graph
+        elif attribute == Attribute.location_occurrence:
+            self.location_occurrence = graph
+        elif attribute == Attribute.material:
+            self.material = graph
+        elif attribute == Attribute.type_of_basic_element:
+            self.type_of_basic_element = graph
+        elif attribute == Attribute.design:
+            self.design = graph
+        elif attribute == Attribute.color_concept:
+            self.color_concept = graph
         else:
             raise Exception("Unkown attribute \"" + attribute + "\"")
         return
@@ -266,6 +334,13 @@ class Taxonomie(Singleton):
         self.plot(Attribute.genre, display)
         self.plot(Attribute.job, display)
         self.plot(Attribute.times_of_day, display)
+        self.plot(Attribute.times_of_play, display)
+        self.plot(Attribute.role, display)
+        self.plot(Attribute.location_occurrence, display)
+        self.plot(Attribute.material, display)
+        self.plot(Attribute.type_of_basic_element, display)
+        self.plot(Attribute.design, display)
+        self.plot(Attribute.color_concept, display)
 
         Logger.normal("All taxonomies have been saved as dot and svg to " + self.plot_directory)
         return
