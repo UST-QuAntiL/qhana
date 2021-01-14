3c0eda5f3b52aad0ec64e6f449b1e9c8aeb61b0f
Daniel Fink <daniel-fink@outlook.com>
2021-01-14 23:21:26 +0800

    Added sklearn clustering.

--- microservices/clustering/test.py
+++ microservices/clustering/test.py
@@ -6,6 +6,7 @@ Email: daniel-fink@outlook.com
 from qiskit import Aer
 from negativeRotation import NegativeRotation
 from destructiveInterference import DestructiveInterference
+from sklearnClustering import SklearnClustering
 from kmeansClusteringAlgorithm import standardize, normalize, generate_random_data
 import matplotlib.pyplot as plt
 import asyncio
@@ -112,7 +113,8 @@ async def main():
     data = generate_random_data(amount_of_fake_data)
     data_preprocessed = normalize(standardize(data))
     # algorithm = NegativeRotation(backend, max_qubits, shots_per_circuit, k, max_runs, eps, base_vector=[.5, .5])
-    algorithm = DestructiveInterference(backend, max_qubits, shots_per_circuit, k, max_runs, eps, base_vector=[.5, .5])
+    # algorithm = DestructiveInterference(backend, max_qubits, shots_per_circuit, k, max_runs, eps, base_vector=[.5, .5])
+    algorithm = SklearnClustering(k, max_runs, eps)
     result = algorithm.perform_clustering(data)
     print(result)
     plot(data, data_preprocessed, result, k)
